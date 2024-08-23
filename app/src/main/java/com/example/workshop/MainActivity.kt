24d289a795ef2d2fe58963aebf709ae58d622f2e
Mats Hansen <notrux93@gmail.com>
2024-08-23 10:06:31 +0200

    something

--- app/src/main/java/com/example/workshop/MainActivity.kt
+++ app/src/main/java/com/example/workshop/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             WorkshopTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Mats",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -33,7 +33,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
-        text = "Hello $name!",
+        text = "Hellooo $name!",
         modifier = modifier
     )
 }
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     WorkshopTheme {
-        Greeting("Android")
+        Greeting("Androiddd")
     }
 }
