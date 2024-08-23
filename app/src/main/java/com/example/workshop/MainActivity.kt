07b9716dacb19425696739f73ba08031b2c7adae
Mats Hansen <notrux93@gmail.com>
2024-08-23 10:05:45 +0200

    created counter functionality

--- app/src/main/java/com/example/workshop/MainActivity.kt
+++ app/src/main/java/com/example/workshop/MainActivity.kt
@@ -4,13 +4,26 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.workshop.ui.theme.WorkshopTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +33,49 @@ class MainActivity : ComponentActivity() {
         setContent {
             WorkshopTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    CounterScreen(modifier = Modifier
+                        .padding(innerPadding)
+                        .fillMaxSize())
                 }
             }
         }
     }
 }
 
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun CounterScreen (modifier: Modifier = Modifier) {
+
+    var count by remember { mutableStateOf(0) }
+
+    Column (
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "$count", fontSize = 150.sp)
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Row (horizontalArrangement = Arrangement.spacedBy(16.dp)) {
+            Button(onClick = { count = if (count == 0) 0 else count - 1 }) {
+                Text(text = "-")
+            }
+            Button(onClick = { count++ }) {
+                Text(text = "+")
+            }
+        }
+
+        Button(onClick = { count = 0 }) {
+            Text(text = "Reset")
+        }
+
+    }
+
 }
 
-@Preview(showBackground = true)
+@Preview (showBackground = true)
 @Composable
-fun GreetingPreview() {
-    WorkshopTheme {
-        Greeting("Android")
-    }
+fun CounterScreenPreview(){
+    CounterScreen()
 }
